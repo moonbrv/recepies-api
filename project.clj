cfcd1aad2c00075b8480db6684edf54d885ba31a
Dmytro Palamarchuk <dmytro.p@attendify.com>
2020-11-15 14:06:40 +0200

    initial app setup for heroku

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject recipes-api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.2"]
+                 [integrant "0.8.0"]
+                 [environ "1.2.0"]
+                 [metosin/reitit "0.5.10"]
+                 [seancorfield/next.jdbc "1.1.613"]
+                 [org.postgresql/postgresql "42.2.18"]
+                 [clj-http "3.10.3"]
+                 [ovotech/ring-jwt "1.3.0"]]
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:source-paths ["dev/src"]
+                   :resource-paths ["dev/resources"]
+                   :dependencies [[ring/ring-mock "0.4.0"]
+                                  [integrant/repl "0.3.2"]]}}
+  :uberjar-name "recipes.jar")
