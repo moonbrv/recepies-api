e9bcca2b91611267b2532b875f2c3a14536c3405
Dmitry Palamarchuk <moonbrv@gmail.com>
2021-04-18 18:21:48 +0300

    add tests for recipe

--- test/recepies_api/recepies_test.clj
+++ test/recepies_api/recepies_test.clj
@@ -18,3 +18,26 @@
         (is (= 200 status))
         (is (vector? (:public body)))
         (is (nil? (:drafts body)))))))
+
+(def recipe
+  {:img "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg"
+   :prep-time 30
+   :name "Pizza Margarita"})
+
+(def update-recipe
+  (assoc recipe :public true))
+
+(deftest recipe-test
+  (let [recipe-id (atom nil)]
+    (testing "create recipe"
+      (let [{:keys [status body]} (st/test-endpoint :post "/v1/recipes" {:auth true
+                                                                         :body recipe})]
+        (is (= status 201))
+        (reset! recipe-id (:recipe-id body))))
+    (testing "update recipe"
+      (let [{:keys [status]} (st/test-endpoint :put (str "/v1/recipes/" @recipe-id) {:auth true
+                                                                                     :body update-recipe})]
+        (is (= status 204))))
+    (testing "delete recipe"
+      (let [{:keys [status]} (st/test-endpoint :delete (str "/v1/recipes/" @recipe-id) {:auth true})]
+        (is (= status 204))))))
